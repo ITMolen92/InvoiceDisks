7c5c252f6a7b63bf8cc51cab822d51871250d276
IT-Molen <IT-Molen@DESKTOP-IRPU2Q0>
2018-09-18 22:09:47 -0700

    Add project files.

---
+++ Invoie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Invoie.Startup))]
+namespace Invoie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
